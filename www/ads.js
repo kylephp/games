84aa375c33c650d501aeecedc242ce8d88d0facf
Kyle Hoang <kyle.hoang@noria.no>
2018-10-16 00:02:36 +0700

    improve loading time

--- www/ads.js
+++ www/ads.js
@@ -1,12 +1,12 @@
 var admobid = {};
 if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
     admobid = {
-        banner: 'ca-app-pub-8215613913865134/5209617703',
+        banner: 'ca-app-pub-8215613913865134/8651832366',
         interstitial: 'ca-app-pub-8215613913865134/6055795515'
     }
 } else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
     admobid = {
-        banner: 'ca-app-pub-8215613913865134/5209617703',
+        banner: 'ca-app-pub-8215613913865134/8651832366',
         interstitial: 'ca-app-pub-8215613913865134/6055795515'
     }
 }
